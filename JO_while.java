ac6dfe15c778fbe7095d317f38eab34089d03960
Kam1kaze54 <matheo20040706@gmail.com>
2024-05-14 23:03:03 -0500

    letras JO con bucle while

---
+++ JO_while.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;  
+
+public class JO_while {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);  
+
+        o
+        System.out.print("Ingrese la altura de las letras 'J' y 'O': ");
+        int altura = scanner.nextInt();
+
+        int i = 0;
+        while (i < altura) {
+            
+            if (i == 0) {
+                
+                int j = 0;
+                while (j < altura - 1) {
+                    System.out.print("*");
+                    j++;
+                }
+                System.out.print("  ");  
+            } else if (i == altura - 1) {
+                
+                System.out.print(" *");
+                int j = 1;
+                while (j < altura - 1) {
+                    System.out.print(" ");
+                    j++;
+                }
+                System.out.print("  ");  
+            } else {
+                
+                int j = 0;
+                while (j < altura - 1) {
+                    System.out.print(" ");
+                    j++;
+                }
+                System.out.print("*  ");  
+            }
+
+            
+            if (i == 0 || i == altura - 1) {
+                
+                System.out.print(" ");
+                int j = 0;
+                while (j < altura - 2) {
+                    System.out.print("*");
+                    j++;
+                }
+            } else {
+                System.out.print("*");
+                int j = 0;
+                while (j < altura - 2) {
+                    System.out.print(" ");
+                    j++;
+                }
+                System.out.print("*");
+            }
+
+            System.out.println();  
+            i++;
+        }
+
+        
+        scanner.close();
+    }
+}
