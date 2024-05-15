bd9cd82c281e3db5f094167d4e1fb81a96cc6b34
Kam1kaze54 <matheo20040706@gmail.com>
2024-05-14 23:57:56 -0500

    letras J y o usando do while

---
+++ do_while.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class do_while {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ingrese la altura de las letras 'J' y 'O': ");
+        int altura = scanner.nextInt();
+        
+        if (altura < 3) {
+            System.out.println("La altura debe ser al menos 3 para formar las letras 'J' y 'O'.");
+            return;
+        }
+        
+        int i = 0;
+        
+        do {
+            if (i == 0) {
+                for (int j = 0; j < altura - 1; j++) {
+                    System.out.print("*");
+                }
+                System.out.print("  ");  
+            } else if (i == altura - 1) {
+                System.out.print(" *");
+                for (int j = 1; j < altura - 1; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("  ");  
+            } else {
+                for (int j = 0; j < altura - 1; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*  ");  
+            }
+
+            if (i == 0 || i == altura - 1) {
+                System.out.print(" ");
+                for (int j = 0; j < altura - 2; j++) {
+                    System.out.print("*");
+                }
+            } else {
+                System.out.print("*");
+                for (int j = 0; j < altura - 2; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+
+            System.out.println();  
+            i++;
+        } while (i < altura);
+
+        scanner.close();
+    }
+}
