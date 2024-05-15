4d7f51486979aa6e1169395c421bcf00f134bbfc
Kam1kaze54 <matheo20040706@gmail.com>
2024-05-15 00:02:48 -0500

    letra O usando bucles do while

--- do__while.java
+++ do__while.java
@@ -1,33 +1,15 @@
 import java.util.Scanner;
 
-public class do_while {
+public class do__while {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.print("Ingrese la altura de las letras 'J' y 'O': ");
+        System.out.print("Ingrese la altura de la letra 'O': ");
         int altura = scanner.nextInt();
         
         int i = 0;
         
         do {
-            if (i == 0) {
-                for (int j = 0; j < altura - 1; j++) {
-                    System.out.print("*");
-                }
-                System.out.print("  ");  
-            } else if (i == altura - 1) {
-                System.out.print(" *");
-                for (int j = 1; j < altura - 1; j++) {
-                    System.out.print(" ");
-                }
-                System.out.print("  ");  
-            } else {
-                for (int j = 0; j < altura - 1; j++) {
-                    System.out.print(" ");
-                }
-                System.out.print("*  ");  
-            }
-
             if (i == 0 || i == altura - 1) {
                 System.out.print(" ");
                 for (int j = 0; j < altura - 2; j++) {
